a08799f66d1efcd5f2f3d6d1cdffc25fe6f20b4d
884kaito <kaito.hayashi@gmail.com>
2025-05-12 18:03:43 -0300

    animate dedede/waddle dee

--- Projeto2/objetos/dedede/dedede.py
+++ Projeto2/objetos/dedede/dedede.py
@@ -8,7 +8,7 @@ class Dedede(Objeto):
 
         self.loader = loader
 
-        #cria os partes do waddle
+        #cria os partes do dedede
         self.body = Objeto(loader)
         self.body.load_model("objetos/dedede/body.obj", "objetos/dedede/dedede.png")
         self.children.append(self.body)
@@ -33,35 +33,64 @@ class Dedede(Objeto):
 
         self.hammer = Objeto(loader)
         self.hammer.load_model("objetos/dedede/hammer.obj", "objetos/dedede/hammer.png")
-        self.children.append(self.hammer)
+        self.left_hand.children.append(self.hammer)
 
 
         
 
-        #####parametros
+        #####parametros editaveis
+
         #transformacao inicial
         self.scale = glm.vec3(3)
 
-        # #walk
-        # self.walk_anim_speed = 1
-        # self.foot_rotation_range = [-30, 50]
-        # self.arm_rotation_range = [-25, 25] 
-        # self.body_rotation_range = [-5, 5]
-        # self.body_bounce_range   = [0.9, 1.1]
-        # self.right_foot.center.y = 0.75
-        # self.left_foot.center.y = 0.75
-        # self.body.center.y = 0.2
+        #walk animation
+        self.walk_anim_speed = 1.6
+        self.foot_rotation_range = [-30, 50]
+        self.arm_rotation_range = [-25, 25] 
+        self.body_rotation_range = [-5, 5]
+        self.body_bounce_range   = [0.9, 1.1]
+        self.right_foot.center.y = 0.75
+        self.left_foot.center.y = 0.75
+        self.body.center.y = 0.2
 
-        # ######variaveis
-        # self.left_foot.rotation_axis = glm.vec3(1,0,0)
-        # self.right_foot.rotation_axis = glm.vec3(1, 0, 0)
+        #walk translation and rotation
+        self.circle_center = glm.vec3(0.0, 0.0, 0.0)
+        self.circle_radius = 50
+        self.inicial_angle = math.radians(0) #angulo inicial em relacao a si mesmo
+        self.circle_angle  = math.radians(-20.0) #angulo de rotacao em relacao ao centro do circulo
+        self.circle_speed  = math.radians(25.0) #degrees per second
 
-        # self.left_hand.rotation_axis = glm.vec3(0, 1, 0)
-        # self.right_hand.rotation_axis = glm.vec3(0, 1, 0)
+        #hammer
+        self.left_hand.center = glm.vec3(0.75, 1.1, 0.26)
 
-        # self.body.rotation_axis = glm.vec3(0, 1, 0)
 
-        # self.walk_time = 0.0
+
+
+
+        ######variaveis
+        self.left_foot.rotation_axis = glm.vec3(1,0,0)
+        self.right_foot.rotation_axis = glm.vec3(1, 0, 0)
+
+        self.left_hand.rotation_axis = glm.vec3(1, 0, 0)
+        self.right_hand.rotation_axis = glm.vec3(0, 1, 0)
+
+        self.body.rotation_axis = glm.vec3(0, 1, 0)
+
+        self.walk_time = 0.0
+
+
+
+
+        #rotacao e translacao inicial
+        self.translation = glm.vec3(
+            self.circle_center.x + self.circle_radius * math.cos(self.circle_angle),
+            self.circle_center.y,
+            self.circle_center.z + self.circle_radius * math.sin(self.circle_angle)
+        )
+        dir_x0 = -math.sin(self.circle_angle)
+        dir_z0 =  math.cos(self.circle_angle)
+        self.rotation_axis = glm.vec3(0,1,0)
+        self.rotation_angle = math.degrees(math.atan2(dir_x0, dir_z0) + self.inicial_angle)
         
 
         #aplica transformacao
@@ -69,9 +98,19 @@ class Dedede(Objeto):
 
 
 
-
+    #faz a animacao e o movimento de andar
     def walk(self, delta_time: float):
-        # 1) Atualiza o tempo de animação
+
+        self.walk_anim(delta_time)
+
+        self.walk_move(delta_time)
+
+        self.update_transform()
+
+
+    #calcula a animacao de andar
+    def walk_anim(self, delta_time: float):
+         # 1) Atualiza o tempo de animação
         self.walk_time += delta_time * self.walk_anim_speed
 
         # 2) Cálculo para os pés
@@ -103,7 +142,6 @@ class Dedede(Objeto):
         self.right_foot.rotation_angle = off_f -  amp_f * seno
 
         # 8) Ângulos dos braços (em fase)
-        self.left_hand.rotation_angle  = off_a -  amp_a * seno
         self.right_hand.rotation_angle = off_a -  amp_a * seno
 
         # 9) Ângulo de rotação do corpo (balanço suave)
@@ -113,9 +151,28 @@ class Dedede(Objeto):
         bounce_scale = off_s + amp_s * seno_double
         self.body.scale = glm.vec3(1, bounce_scale, 1)
 
-        # 11) Propaga transformações na hierarquia
-        self.update_transform()
     
+
+    #calcula o movimento de andar
+    def walk_move(self, delta_time: float):
+        # 1) atualiza ângulo ao longo do tempo
+        self.circle_angle += delta_time * self.circle_speed
+
+        # 2) calcula nova posição ao longo do círculo
+        x = self.circle_center.x + self.circle_radius * math.cos(self.circle_angle)
+        z = self.circle_center.z + self.circle_radius * math.sin(self.circle_angle)
+        self.translation = glm.vec3(x, self.translation.y, z)
+
+        # 3) orienta o objeto para a tangente do círculo
+        dir_x = -math.sin(self.circle_angle)
+        dir_z =  math.cos(self.circle_angle)
+        self.rotation_axis = glm.vec3(0,1,0)
+        self.rotation_angle = math.degrees(math.atan2(dir_x, dir_z) + self.inicial_angle)
+
+
+
+
+    #reseta os parametros de animacao de andar
     def stop_walk(self):
         self.left_foot.rotation_angle = 0
         self.right_foot.rotation_angle = 0
@@ -125,4 +182,14 @@ class Dedede(Objeto):
         self.body.scale = glm.vec3(1)
 
         self.update_transform()
+
+
+    
+    def lower_hammer(self):
+            self.left_hand.rotation_angle = 125.0
+            self.update_transform()
+
+    def raise_hammer(self):
+            self.left_hand.rotation_angle = 0.0
+            self.update_transform()
 
--- Projeto2/objetos/waddle_dee/waddle_dee.py
+++ Projeto2/objetos/waddle_dee/waddle_dee.py
@@ -46,12 +46,13 @@ class WaddleDee(Objeto):
 
 
 
-        #####parametros
-        #transformacao inicial
-        self.scale = glm.vec3(4)
+        #####parametros editaveis
 
-        #walk
-        self.walk_anim_speed = 1
+        #transformacao inicial
+        self.scale = glm.vec3(3)
+
+        #walk animation
+        self.walk_anim_speed = 2.3
         self.foot_rotation_range = [-30, 50]
         self.arm_rotation_range = [-25, 25] 
         self.body_rotation_range = [-5, 5]
@@ -59,6 +60,17 @@ class WaddleDee(Objeto):
         self.right_foot.center.y = 0.75
         self.left_foot.center.y = 0.75
         self.body.center.y = 0.2
+
+        #walk translation and rotation
+        self.circle_center = glm.vec3(0.0, 0.0, 0.0)
+        self.circle_radius = 50
+        self.inicial_angle = math.radians(0) #angulo inicial em relacao a si mesmo
+        self.circle_angle  = math.radians(0.0) #angulo de rotacao em relacao ao centro do circulo
+        self.circle_speed  = math.radians(25.0) #degrees per second
+
+
+
+
 
         ######variaveis
         self.left_foot.rotation_axis = glm.vec3(1,0,0)
@@ -70,6 +82,20 @@ class WaddleDee(Objeto):
         self.body.rotation_axis = glm.vec3(0, 1, 0)
 
         self.walk_time = 0.0
+
+
+
+
+        #rotacao e translacao inicial
+        self.translation = glm.vec3(
+            self.circle_center.x + self.circle_radius * math.cos(self.circle_angle),
+            self.circle_center.y,
+            self.circle_center.z + self.circle_radius * math.sin(self.circle_angle)
+        )
+        dir_x0 = -math.sin(self.circle_angle)
+        dir_z0 =  math.cos(self.circle_angle)
+        self.rotation_axis = glm.vec3(0,1,0)
+        self.rotation_angle = math.degrees(math.atan2(dir_x0, dir_z0) + self.inicial_angle)
         
 
         #aplica transformacao
@@ -78,8 +104,19 @@ class WaddleDee(Objeto):
 
 
 
+    #faz a animacao e o movimento de andar
     def walk(self, delta_time: float):
-        # 1) Atualiza o tempo de animação
+
+        self.walk_anim(delta_time)
+
+        self.walk_move(delta_time)
+
+        self.update_transform()
+
+
+    #calcula a animacao de andar
+    def walk_anim(self, delta_time: float):
+         # 1) Atualiza o tempo de animação
         self.walk_time += delta_time * self.walk_anim_speed
 
         # 2) Cálculo para os pés
@@ -121,9 +158,28 @@ class WaddleDee(Objeto):
         bounce_scale = off_s + amp_s * seno_double
         self.body.scale = glm.vec3(1, bounce_scale, 1)
 
-        # 11) Propaga transformações na hierarquia
-        self.update_transform()
     
+
+    #calcula o movimento de andar
+    def walk_move(self, delta_time: float):
+        # 1) atualiza ângulo ao longo do tempo
+        self.circle_angle += delta_time * self.circle_speed
+
+        # 2) calcula nova posição ao longo do círculo
+        x = self.circle_center.x + self.circle_radius * math.cos(self.circle_angle)
+        z = self.circle_center.z + self.circle_radius * math.sin(self.circle_angle)
+        self.translation = glm.vec3(x, self.translation.y, z)
+
+        # 3) orienta o objeto para a tangente do círculo
+        dir_x = -math.sin(self.circle_angle)
+        dir_z =  math.cos(self.circle_angle)
+        self.rotation_axis = glm.vec3(0,1,0)
+        self.rotation_angle = math.degrees(math.atan2(dir_x, dir_z) + self.inicial_angle)
+
+
+
+
+    #reseta os parametros de animacao de andar
     def stop_walk(self):
         self.left_foot.rotation_angle = 0
         self.right_foot.rotation_angle = 0
